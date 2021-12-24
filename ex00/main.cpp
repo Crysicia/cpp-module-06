5c0a39004c4c1441588fb2717179f23087b3d90c
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-12-24 11:39:14 +0100

    Add format detection

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -10,18 +10,36 @@ class Convert {
 #define F_INT 1
 #define F_FLOAT 2
 #define F_DOUBLE 3
+#define F_CHAR 4
+
+bool isCorrectlyFormatted(const char c, const size_t index, const size_t string_length) {
+	static const std::string charset = "0123456789-.f";
+
+	if (charset.find(c) == std::string::npos) { return false; }
+	if (index == 0 && (!std::isdigit(c) && c != '-')) { return false; }
+	if (index == 1 && (!std::isdigit(c) && c != '.')) { return false; }
+	if (c == '-' && (index != 0 || index == string_length - 1)) { return false; }
+	if (c == 'f' && (index != string_length - 1 || index == 0)) { return false; }
+	return true;
+}
+
 
 int getType(const std::string &input) {
 	size_t dot_count = 0;
 	size_t string_length = input.length();
+	const char last_char = input[string_length - 1];
 
+
+	if (string_length == 1 && std::isprint(last_char)) { return F_CHAR; }
 	for (size_t i = 0; i < string_length; i++) {
-		if (!std::isdigit(input[i]) && input[i] != '.' && ((i != string_length - 1) && input[i] == 'f')) { return F_UNKNOWN; }
+		if (!isCorrectlyFormatted(input[i], i, string_length)) { return F_UNKNOWN; }
 		if (input[i] == '.') { dot_count++; }
 	}
-	if (input[string_length - 1] == 'f' && dot_count == 1) { return F_FLOAT; }
-	if (input[string_length - 1] != 'f' && dot_count == 1) { return F_DOUBLE; }
-	if (input[string_length - 1] != 'f' && dot_count == 0) { return F_INT; }
+	if (last_char != 'f' && dot_count == 0) { return F_INT; }
+	if (dot_count != 1) { return F_UNKNOWN; }
+	if (!std::isdigit(input[input.rfind('.') + 1])) { return F_UNKNOWN; }
+	if (last_char == 'f') { return F_FLOAT; }
+	if (last_char != 'f') { return F_DOUBLE; }
 	return F_UNKNOWN;
 }
 
